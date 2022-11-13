5df078ce881ac14427c9b57bc75d5c307218ce73
dimsplendid <dimsplendid@gmail.com>
2022-11-13 20:31:28 +0800

    Get data from server

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Observable, of } from 'rxjs';
 
@@ -12,11 +13,24 @@ import { MessageService } from './message.service';
 export class HeroService {
 
   constructor(
-    private messageService: MessageService
+    private messageService: MessageService,
+    private http: HttpClient
   ) { }
+
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
+  private heroUrl = 'http://127.0.0.1:8000/hero/';  // URL to web api
+
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
     this.messageService.add('HeroService: fetched heroes');
-    return heroes;
+    return this.http.get<Hero[]>(this.heroUrl+'list', { responseType: 'json' });
+  }
+  getHero(id: number): Observable<Hero> {
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return this.http.get<Hero>(this.heroUrl + String(id), {
+      responseType: 'json',
+    });
   }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -19,16 +19,10 @@ export class HeroesComponent implements OnInit {
 
   constructor(
     private heroService: HeroService,
-    private messageService: MessageService
   ) { }
 
   ngOnInit(): void {
     this.getHeroes();
-  }
-  
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   getHeroes(): void {
